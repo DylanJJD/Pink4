9154f899a2ce97c1a35466efdd52bd3c94cafab7
asd <zizic1234@naver.com>
2024-07-26 17:50:55 +0900

    123qweasd

--- Pink4/src/Four/Pink4.java
+++ Pink4/src/Four/Pink4.java
@@ -2,11 +2,7 @@ package Four;
 
 public class Pink4 {
 	public static void main(String[] args) {
-		System.out.println("we are pinky");
-		System.out.println("we are pinky");
-		System.out.println("we are pinky");
-		System.out.println("we are pinky");
-		System.out.println("we are pinky");
+		System.out.println("qweasd");
 		
 	}
 }
