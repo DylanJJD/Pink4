d202956d3ababc9eda316c2fcbe98f8d204494ff
user1 <user1@DESKTOP-CP1IEJ4>
2024-07-26 17:33:12 +0900

    첫번째 시도

---
+++ Pink4/src/Four/Pink4.java
@@ -0,0 +1,9 @@
+package Four;
+
+public class Pink4 {
+	public static void main(String[] args) {
+		System.out.println("we are pinky");
+		System.out.println("we are pinky");
+		System.out.println("we are pinky");
+	}
+}
