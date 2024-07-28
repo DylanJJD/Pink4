f2ceb27e3ead5566b7c9e4b117972ae159c630b7
user1 <user1@DESKTOP-C2J4RUM>
2024-07-29 08:57:20 +0900

    123qweqweqe

--- Pink4/src/Four/TestClass.java
+++ Pink4/src/Four/TestClass.java
@@ -3,5 +3,8 @@ package Four;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
 	}
 }
