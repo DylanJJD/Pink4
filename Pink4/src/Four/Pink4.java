b4e3aeb36712a9f264e789ea0a8a2a5dd9f84062
DJ <user1@DESKTOP-C2J4RUM>
2024-07-29 08:59:03 +0900

    DJ

--- Pink4/src/Four/Pink4.java
+++ Pink4/src/Four/Pink4.java
@@ -9,5 +9,11 @@ public class Pink4 {
 		System.out.println("we are pinky");
 		System.out.println("we are pinky");
 		System.out.println("박영빈 업로드 테스트");
+		System.out.println("we are pinky");
+		System.out.println("we are pinky");
+		System.out.println("박영빈 업로드 테스트");
+		System.out.println("we are pinky");
+		System.out.println("we are pinky");
+		System.out.println("박영빈 업로드 테스트");
 	}
 }
